bd90e8eb62ced2ba66e6d50bb39e518ef625aa30
Jackson Ludwig <ludwj61@mail.sunysuffolk.edu>
2019-11-12 12:45:07 -0500

    ver 1 done

--- Journal/p1/main.py
+++ Journal/p1/main.py
@@ -1,7 +1,9 @@
 import datetime
 import os
 
-author = "testAuthor"
+MAX_CHAR_PER_LINE = 13
+
+author = input("Enter author name: ")
 
 
 def create_journal():
@@ -41,14 +43,7 @@ def enter_entry_content():
     return info
 
 
-def enter_entry_supplemental():
-    title = input("Enter a title for this entry:\n")
-    content = enter_entry_content()
-    file = title.replace(" ", "") + ".txt"
-    entry = open(file, "a")
-    entry.write(author + "\n")
-    entry.write(title + "\n")
-
+def write_to_file(entry, content):
     char_count = 0
     word_start_index = 0
     for i in range(0, len(content) - 1):
@@ -56,14 +51,60 @@ def enter_entry_supplemental():
             entry.write(content[word_start_index:i])
             char_count += 1
             word_start_index = i
-        if char_count == 10:
+        if char_count == MAX_CHAR_PER_LINE:
             char_count = 0
             entry.write("\n")
 
-    entry.write("\n" + str(datetime.datetime.now()))
     entry.close()
 
 
-create_journal()
-view_journal()
-enter_entry_supplemental()
+def enter_entry_supplemental_with_title(title):
+    content = enter_entry_content()
+    file = title.replace(" ", "") + ".txt"
+    entry = open(file, "a")
+    entry.write(author + "\n")
+    entry.write(title + "\n")
+    entry.write(str(datetime.datetime.now()) + "\n")
+
+    write_to_file(entry, content)
+
+
+def add_page():
+    title = input("Enter a name for the new journal entry:\n")
+    enter_entry_supplemental_with_title(title)
+
+
+def remove_page():
+    title = input("Enter the title of the entry you want to delete:\n")
+    file = title.replace(" ", "")
+    os.remove(file)
+
+
+def use_journal():
+    print("Journal Controls: ")
+    print("1: Make a new journal")
+    print("2: Open an existing journal")
+    print("3: Create a new entry in an existing journal")
+    print("4: Add on to an existing entry")
+    print("5: Remove an existing entry")
+
+    while True:
+        menu_choice = input("Type your choice here: ")
+        print("--------------------")
+        if menu_choice is 1 or menu_choice is "1":
+            create_journal()
+        elif menu_choice is 2 or menu_choice is "2":
+            view_journal()
+        elif menu_choice is 3 or menu_choice is "3":
+            add_page()
+        elif menu_choice is 4 or menu_choice is "4":
+            enter_entry_content()
+        elif menu_choice is 5 or menu_choice is "5":
+            remove_page()
+        elif menu_choice is 6 or menu_choice is "6":
+            break
+        else:
+            print("Only enter given options")
+
+
+use_journal()
