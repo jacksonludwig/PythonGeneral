302002d750b02e28698a5c946d735989787d20b8
Jackson <jacksonludwig74@yahoo.com>
2019-12-26 01:26:52 -0500

    rename

--- Intro/RenameFilesToNumbers/renameFiles.py
+++ Intro/RenameFilesToNumbers/renameFiles.py
@@ -1,7 +1,7 @@
 import os
 
 path = 'D:/DiscordBot/images'
-i = 0
+i = 1
 for filename in os.listdir(path):
     os.rename(os.path.join(path, filename), os.path.join(path, str(i) + ".png"))
     i = i + 1
